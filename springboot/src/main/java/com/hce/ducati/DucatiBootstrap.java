fdbc6ca316a6a969b45b66d7a21655832d3e58ab
Qiang Ma <mq7253180@126.com>
2019-11-19 17:33:13 +0800

    xxx

--- springboot/src/main/java/com/hce/ducati/DucatiBootstrap.java
+++ springboot/src/main/java/com/hce/ducati/DucatiBootstrap.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -20,7 +20,7 @@ import com.quincy.sdk.Constants;
 
 //@EnableDiscoveryClient
 //@EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
 @MapperScan(basePackages = {AuthConstants.PACKAGE_NAME_MAPPER, "com.hce.ducati.mapper"})
 @EntityScan(basePackages = {Constants.PACKAGE_NAME_ENTITY, AuthConstants.PACKAGE_NAME_ENTITY, "com.hce.ducati.entity"})
 @EnableJpaRepositories(basePackages = {Constants.PACKAGE_NAME_REPOSITORY, AuthConstants.PACKAGE_NAME_REPOSITORY, "com.hce.ducati.dao"})
