9e87e95312db125d1a627caa62ee99f543bc7042
Qiang Ma <mq7253180@126.com>
2019-11-25 18:10:16 +0800

    xxx

--- springboot/src/main/java/com/hce/ducati/SpringBootConfiguration.java
+++ springboot/src/main/java/com/hce/ducati/SpringBootConfiguration.java
@@ -3,9 +3,12 @@ package com.hce.ducati;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.hce.ducati.freemarker.PaginationTemplateDirectiveModelBean;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 //@PropertySource("classpath:application-springboot.properties")
 @Configuration
@@ -17,5 +20,19 @@ public class SpringBootConfiguration {
     @PostConstruct
     public void init() {
     		configuration.setSharedVariable("p", new PaginationTemplateDirectiveModelBean());
+    }
+
+//    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+    		HystrixMetricsStreamServlet streamServlet =new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>(streamServlet);
+    		/*ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<HystrixMetricsStreamServlet>();
+        registrationBean.setServlet(streamServlet);*/
+        registrationBean.setEnabled(true);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+//        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
