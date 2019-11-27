4f2f94f48f08a842d9c59b76d07196fbee4868b2
Qiang Ma <mq7253180@126.com>
2019-11-27 19:05:00 +0800

    xxx

--- service/src/main/java/com/hce/ducati/controller/XxxController.java
+++ service/src/main/java/com/hce/ducati/controller/XxxController.java
@@ -3,6 +3,7 @@ package com.hce.ducati.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,5 +54,14 @@ public class XxxController {
 
 	public String hystrixFailure() {
 		return "FAILURE";
+	}
+
+	@Value("${ducati.host}")
+	private String host;
+
+	@GetMapping("/host")
+	@ResponseBody
+	public String host() {
+		return host;
 	}
 }
