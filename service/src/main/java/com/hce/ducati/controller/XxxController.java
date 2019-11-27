c3c9ef67fbb7ee41c85ec8d553eae769331e1a1d
Qiang Ma <mq7253180@126.com>
2019-11-27 23:48:12 +0800

    xxx

--- service/src/main/java/com/hce/ducati/controller/XxxController.java
+++ service/src/main/java/com/hce/ducati/controller/XxxController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.quincy.sdk.entity.Region;
 
+@RefreshScope
 @Controller
 @RequestMapping("/xxx")
 public class XxxController {
