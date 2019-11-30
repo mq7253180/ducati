f8a5fd965f3193bd949eca6fe862586992302453
Qiang Ma <mq7253180@126.com>
2019-11-30 22:04:12 +0800

    xxx

---
+++ service/src/main/java/com/hce/ducati/controller/AmqpController.java
@@ -0,0 +1,90 @@
+package com.hce.ducati.controller;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/amqp")
+public class AmqpController {
+	private final static ConnectionFactory connectionFactory = new ConnectionFactory();
+	private static Connection conn = null;
+	private static Channel channel = null;
+	private final static String QUEUE_NAME = "queue_test";
+	private final static String ROUTING_KEY = QUEUE_NAME.substring(QUEUE_NAME.indexOf("_")+1, QUEUE_NAME.length());
+	private final static String EXCHANGE_NAME = "exchange_test";
+	static {
+		connectionFactory.setHost("47.93.89.0");
+		connectionFactory.setUsername("guest");
+		connectionFactory.setPassword("guest");
+		try {
+			conn = connectionFactory.newConnection();
+			channel = conn.createChannel(1);
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+			channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+			channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+		} catch (Exception e) {
+			log.error("AMQP_ERR: ", e);
+		}
+	}
+
+	@RequestMapping("/send")
+	@ResponseBody
+	public void send(@RequestParam(required = true, name = "content")String content) throws IOException, TimeoutException {
+		Channel channel = null;
+		try {
+			channel = conn.createChannel(2);
+//			channel.basicPublish("", QUEUE_NAME, null, content.getBytes());
+			channel.basicPublish(EXCHANGE_NAME, "xxx", null, content.getBytes());
+		} finally {
+			if(channel!=null)
+				channel.close();
+		}
+	}
+
+	@PostConstruct
+	public void init() throws IOException, TimeoutException {
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body) throws IOException {
+				super.handleDelivery(consumerTag, envelope, properties, body);
+				log.warn("CONSUMER_MSG======================={}", new String(body, "UTF-8"));
+			}
+		};
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
+
+	@PreDestroy
+	public void destroy() {
+		try {
+			if(channel!=null) {
+				channel.queuePurge(QUEUE_NAME);
+				channel.exchangeDelete(EXCHANGE_NAME);
+				channel.close();
+			}
+			if(conn!=null)
+				conn.close();
+			log.warn("========================AMQP RELEASED");
+		} catch (Exception e) {
+			log.error("AMQP_ERR: ", e);
+		}
+	}
+}
