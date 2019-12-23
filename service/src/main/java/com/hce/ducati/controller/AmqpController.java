116d071aa2408b4f7ad2198192dd71ec8f07e5e1
Qiang Ma <mq7253180@126.com>
2019-12-24 00:35:57 +0800

    xxx

--- service/src/main/java/com/hce/ducati/controller/AmqpController.java
+++ service/src/main/java/com/hce/ducati/controller/AmqpController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Consumer;
@@ -41,13 +42,33 @@ public class AmqpController {
 
 	@RequestMapping("/send")
 	@ResponseBody
-	public void send(@RequestParam(required = true, name = "content")String content) throws IOException, TimeoutException {
+	public void send(@RequestParam(required = true, name = "content")String content) throws IOException, TimeoutException, InterruptedException {
 		Channel channel = null;
 		try {
 			channel = conn.createChannel(2);
+			// 开启发送方确认模式
+			channel.confirmSelect();
 //			channel.basicPublish("", QUEUE_NAME, null, content.getBytes());
 //			channel.basicPublish(EXCHANGE_NAME, "xxx", MessageProperties.PERSISTENT_TEXT_PLAIN, content.getBytes());
 			channel.basicPublish(EXCHANGE_NAME, "xxx", null, content.getBytes());
+			//普通 Confirm 模式
+			if(channel.waitForConfirms())
+			    log.warn("======================消息发送成功" );
+			//批量 Confirm 模式
+			channel.waitForConfirmsOrDie(); //直到所有信息都发布，只要有一个未确认就会IOException
+			log.warn("======================全部执行完成");
+			//异步 Confirm 模式
+			channel.addConfirmListener(new ConfirmListener() {
+				@Override
+				public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+					log.warn("======================已确认消息，标识：{}，多个消息：{}", deliveryTag, multiple);
+				}
+
+				@Override
+				public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+					log.warn("======================未确认消息，标识：{}", deliveryTag);
+				}
+			});
 		} finally {
 			if(channel!=null)
 				channel.close();
@@ -70,9 +91,13 @@ public class AmqpController {
 			public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body) throws IOException {
 				super.handleDelivery(consumerTag, envelope, properties, body);
 				log.warn("CONSUMER_MSG======================={}", new String(body, "UTF-8"));
+				//发布的每一条消息都会获得一个唯一的deliveryTag，deliveryTag在channel范围内是唯一的 
+                long deliveryTag = envelope.getDeliveryTag();
+                //手动确认消息: 第二个参数是批量确认标志。如果值为true，则执行批量确认，此deliveryTag之前收到的消息全部进行确认; 如果值为false，则只对当前收到的消息进行确认
+                channel.basicAck(deliveryTag, false);
 			}
 		};
-		channel.basicConsume(QUEUE_NAME, true, consumer);
+		channel.basicConsume(QUEUE_NAME, true, consumer);//autoAck: 是否自动确认消息, true自动确认, false不自动要手动调用
 	}
 
 	@PreDestroy
