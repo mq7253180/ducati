c611ab5a7d2bde2fadd9ce28a43ea91b3095afa7
Qiang Ma <mq7253180@126.com>
2020-03-10 21:02:38 +0800

    xxx

--- springboot/src/main/static/js/login.js
+++ springboot/src/main/static/js/login.js
@@ -7,13 +7,14 @@ var login = function() {
 		dataType: "JSON",
 		data: {
 			"username": $("#uname").val(),
-			"password": $.md5($("#pwd").val()),
+			"password": $.md5($("#pwd").val())
 		},
 		handle: function(result) {
 			if(result.status==1) {
 //				alert("Auth: "+result.msg+"-------"+result.data.user.name);
 //				$.cookie("JSESSIONID_DUCATI", result.data.jsessionid, {expires: 1, path: "/"});
-				$(location).attr("href", "/index");
+				var backto = $.trim($("#backto").val());
+				$(location).attr("href", backto.length==0?"/index":backto);
 			} else {
 				alert(result.msg);
 				$("#"+(result.status==-2||result.status==-4)?"pwd":"uname").focus();
--- springboot/src/main/static/js/login_broker.js
+++ springboot/src/main/static/js/login_broker.js
@@ -1,2 +1,2 @@
 alert($("#tip").html());
-$(top.location).attr("href", "/auth/signin");
+$(top.location).attr("href", "/auth/signin?backto="+$("#backto").val());
