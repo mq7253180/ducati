3380cfcbdc34abbbbe502d976690eb791f8ed982
Qiang Ma <mq7253180@126.com>
2020-03-11 14:41:11 +0800

    xxx

--- service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
+++ service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
@@ -4,12 +4,17 @@ import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.quincy.auth.AuthContext;
+import com.quincy.auth.AuthHandler;
 import com.quincy.sdk.DTransactionContext;
 import com.quincy.sdk.DTransactionFailure;
 import com.quincy.sdk.EmailService;
@@ -22,6 +27,8 @@ public class ServiceInitConfiguration extends WebMvcConfiguration {
 	private DTransactionContext transactionContext;
 	@Autowired
 	private EmailService emailService;
+	@Autowired
+	private AuthContext authContext;
 
 	@Scheduled(cron = "0 0/2 * * * ?")
 	public void retry() throws ClassNotFoundException, NoSuchMethodException, SecurityException, IOException {
@@ -39,6 +46,12 @@ public class ServiceInitConfiguration extends WebMvcConfiguration {
 			@Override
 			public void inform(String message) {
 				emailService.send("mq7253180@126.com", "转账失败", message, "", null, "UTF-8", null, null);
+			}
+		});
+		authContext.setAuthHandler(new AuthHandler() {
+			@Override
+			public ModelAndView indexView(HttpServletRequest request, HttpServletResponse response) {
+				return new ModelAndView("/index");
 			}
 		});
 	}
