8516ea41924f305514bc86ea7c1ba070cc9d5d4f
mq7253180 <mq7253180@126.com>
2023-07-08 15:21:57 +0800

    xxx

--- service-sharding/src/main/java/com/hce/ducati/dao/TestDao.java
+++ service-sharding/src/main/java/com/hce/ducati/dao/TestDao.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import com.hce.ducati.dto.UserDto;
 import com.quincy.sdk.MasterOrSlave;
-import com.quincy.sdk.annotation.AllShardDao;
+import com.quincy.sdk.annotation.AllShardsJDBCDao;
 import com.quincy.sdk.annotation.ExecuteQuery;
 import com.quincy.sdk.annotation.ExecuteUpdate;
 
-@AllShardDao
+@AllShardsJDBCDao
 public interface TestDao {
 	@ExecuteQuery(sql = "SELECT id AS id_str,id,name,nickname,mobile_phone,creation_time FROM s_user;", masterOrSlave = MasterOrSlave.SLAVE, returnItemType = UserDto.class)
 	public List<UserDto>[] findAllUsers();
