76331d08197dfc1d094f51de48e2b1b59cf5c04d
mq7253180 <mq7253180@126.com>
2023-10-26 10:24:22 +0800

    xxx

---
+++ springboot/src/main/java/com/hce/ducati/HondaController.java
@@ -0,0 +1,19 @@
+package com.hce.ducati;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/honda")
+public class HondaController {
+	@Value("${ducati.host}")
+	private String value;
+
+	@RequestMapping("/host")
+	@ResponseBody
+	public String get() {
+		return this.value;
+	}
+}
