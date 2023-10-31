b4265cb4c6df8d1beb7b0c5c73430f89371b4e2f
mq7253180 <mq7253180@126.com>
2023-10-31 16:06:59 +0800

    xxx

--- springboot/src/main/java/com/hce/ducati/DucatiBootstrap.java
+++ springboot/src/main/java/com/hce/ducati/DucatiBootstrap.java
@@ -1,14 +1,14 @@
 package com.hce.ducati;
 
-//import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -19,9 +19,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.quincy.sdk.Constants;
 
-//@EnableDubbo
+@EnableDubbo
+@EnableFeignClients
 //@EnableHystrix
-//@EnableFeignClients
 //@EnableDiscoveryClient
 //@EnableEurekaClient
 @MapperScan(basePackages = {Constants.PACKAGE_NAME_MAPPER, "com.hce.ducati.mapper"})
