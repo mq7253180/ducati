6c82cdffe30a2ddbfc66510c12fbc8fb7631870b
mq7253180 <mq7253180@126.com>
2024-04-23 16:50:22 +0800

    xxx

--- service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
+++ service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
@@ -1,6 +1,9 @@
 package com.hce.ducati;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -15,28 +18,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.hce.ducati.entity.UserEntity;
+import com.hce.ducati.service.UserService;
 import com.quincy.auth.AuthContext;
 import com.quincy.auth.AuthHandler;
-import com.quincy.core.web.SignatureInterceptor;
-import com.quincy.core.web.SignaturePubKeyExchanger;
+import com.quincy.auth.controller.AuthActions;
+import com.quincy.auth.controller.PwdRestEmailInfo;
+import com.quincy.auth.o.User;
+import com.quincy.core.web.PublicKeyGetter;
+import com.quincy.o.AttributeKeys;
+import com.quincy.o.MyParams;
+import com.quincy.sdk.Client;
 import com.quincy.sdk.DTransactionContext;
 import com.quincy.sdk.DTransactionFailure;
 import com.quincy.sdk.EmailService;
-import com.quincy.sdk.WebMvcConfiguration;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @PropertySource(value = {"classpath:application-core.properties", "classpath:application-auth.properties", "classpath:application-service.properties", "classpath:application-oauth2.properties", "classpath:application-sensitiveness.properties"})
 @Configuration("sssiiiccc")
-public class ServiceInitConfiguration extends WebMvcConfiguration {
+public class ServiceInitConfiguration {
 	@Autowired
 	private DTransactionContext transactionContext;
 	@Autowired
 	private EmailService emailService;
 	@Autowired
 	private AuthContext authContext;
+	@Autowired
+	private UserService userService;
 
 	@Scheduled(cron = "0 0/2 * * * ?")
 	public void retry() throws ClassNotFoundException, NoSuchMethodException, SecurityException, IOException, InterruptedException {
@@ -67,16 +78,76 @@ public class ServiceInitConfiguration extends WebMvcConfiguration {
 		});
 	}
 
-	@Override
-	protected void addInterceptors(InterceptorRegistry registry) {
-		super.addInterceptors(registry);
-		registry.addInterceptor(new SignatureInterceptor(new SignaturePubKeyExchanger() {
+	@Bean
+	public PublicKeyGetter publicKeyGetter() {
+		return new PublicKeyGetter() {
 			@Override
-			public String getPublicKeyById(String id) {
+			public String getById(String id) {
 //				return null;
 				return "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCMYDMqMFSJL+nUMzF7MQjCYe/Y3P26wjVn90CdrSE8H9Ed4dg0/BteWn5+ZK65DwWev2F79hBIpprPrtVe+wplCTkpyR+mPiNL+WKkvo7miMegRYJFZLvh9QrFuDzMJZ+rAiu4ldxkVB0CMKfYEWbukKGmAinxVAqUr/HcW2mWjwIDAQAB";
 			}
-		})).addPathPatterns("/**");
+		};
+	}
+
+	@Bean
+	public PwdRestEmailInfo pwdRestEmailInfo() {
+		return new PwdRestEmailInfo() {
+			@Override
+			public String getSubject() {
+				return "密码重置";
+			}
+
+			@Override
+			public String getContent(String uri) {
+				return new StringBuilder(500)
+						.append("<html>")
+						.append("\n\t<head>")
+						.append("\n\t\t<meta charset=\"UTF-8\">")
+						.append("\n\t\t<title>密码重置</title>")
+						.append("\n\t</head>")
+						.append("\n\t<body>")
+						.append("\n\t\t<font color=\"purple\">请</font>")
+						.append("<a href=\"")
+						.append("http://127.0.0.1:12081")
+						.append(uri)
+						.append("\">点击</a>")
+						.append("<font color=\"purple\">继续操作，如果无法自动打开，请复制此链接至浏览器地址栏敲回车: ")
+						.append("http://127.0.0.1:12081")
+						.append(uri)
+						.append("</font>")
+						.append("\n\t</body>")
+						.append("\n</html>").toString();
+			}
+		};
+	}
+
+	@Bean
+	public AuthActions authActions() {
+		return new AuthActions() {
+			@Override
+			public User findUser(String username, Client client) {
+				UserEntity userEntity = userService.find(username);
+				User user = ControllerUtils.toUser(userEntity);
+//				user.setCurrencyAccounts(new HashMap<String, BigDecimal>(2));
+				user.setAttributes(new HashMap<String, Serializable>(2));
+				user.getAttributes().put(AttributeKeys.MY_PARAMS, new MyParams().setXxx("wwwqqq"));
+				return user;
+			}
+
+			@Override
+			public void updateLastLogin(Long userId, Client client, String jsessionid) {
+				UserEntity vo = new UserEntity();
+				vo.setId(userId);
+				vo.setJsessionid(jsessionid);
+				vo.setLastLogined(new Date());
+				userService.update(vo);
+			}
+
+			@Override
+			public ModelAndView signinView(HttpServletRequest request) {
+				return null;
+			}
+		};
 	}
 
 	@PreDestroy
