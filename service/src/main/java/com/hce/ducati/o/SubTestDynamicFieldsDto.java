b027fdb34b3efa826732d33f268da3c87443e54d
mq7253180 <mq7253180@126.com>
2024-10-27 01:17:30 +0800

    xxx

--- service/src/main/java/com/hce/ducati/o/SubTestDynamicFieldsDto.java
+++ service/src/main/java/com/hce/ducati/o/SubTestDynamicFieldsDto.java
@@ -3,6 +3,7 @@ package com.hce.ducati.o;
 import java.io.Serializable;
 import java.util.List;
 
+import com.quincy.sdk.DynamicField;
 import com.quincy.sdk.annotation.jdbc.DynamicColumnQueryDTO;
 import com.quincy.sdk.annotation.jdbc.DynamicFields;
 import com.quincy.sdk.annotation.jdbc.Result;
@@ -14,7 +15,7 @@ import lombok.Data;
 public class SubTestDynamicFieldsDto implements Serializable {
 	private static final long serialVersionUID = 7953404612544688506L;
 	@DynamicFields
-	private List<String> dynamicFieldNames;
+	private List<DynamicField> dynamicFields;
 	@Result
 	private List<SubTestDto> result;
 }
