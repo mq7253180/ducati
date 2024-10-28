d755c6ef475a2daf2e5ac45b784f2dc577020a3a
mq7253180 <mq7253180@126.com>
2024-10-29 01:26:29 +0800

    xxx

--- service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
+++ service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.quincy.core.PublicKeyGetter;
 import com.quincy.o.AttributeKeys;
