1c04c5ceb226e69fb310e9911cc1e7e0f9b6ee9a
mq7253180 <mq7253180@126.com>
2025-03-19 18:33:59 +0800

    xxx

--- service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
+++ service/src/main/java/com/hce/ducati/ServiceInitConfiguration.java
@@ -3,6 +3,7 @@ package com.hce.ducati;
 import java.io.IOException;
 import java.io.Serializable;
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -17,16 +18,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import com.quincy.auth.entity.Permission;
+import com.quincy.auth.entity.Role;
 import com.quincy.core.PublicKeyGetter;
-import com.quincy.o.AttributeKeys;
-import com.quincy.o.MyParams;
-import com.quincy.sdk.AuthActions;
+import com.quincy.sdk.AuthServerActions;
 import com.quincy.sdk.DTransactionOptRegistry;
 import com.quincy.sdk.DTransactionFailure;
 import com.quincy.sdk.EmailService;
 import com.quincy.sdk.PwdRestEmailInfo;
 import com.quincy.sdk.RootControllerHandler;
 import com.quincy.sdk.TempPwdLoginEmailInfo;
+import com.quincy.sdk.o.Menu;
+import com.quincy.sdk.o.User;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -132,11 +135,35 @@ public class ServiceInitConfiguration {
 	}
 
 	@Bean
-	public AuthActions authActions() {
-		return new AuthActions() {
+	public AuthServerActions authActions() {
+		return new AuthServerActions() {
 			@Override
 			public void sms(String mobilePhone, String vcode, int expireMinuts) {
 				System.out.println(MessageFormat.format("已通过阿里云短信接口向{0}发送验证码: {1}, 失效时间{2}分钟", mobilePhone, new String(vcode), expireMinuts));
+			}
+
+			@Override
+			public List<Role> findRoles(Long userId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public List<Permission> findPermissions(Long userId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public List<Menu> findMenus(Long userId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public <T extends Serializable> T userExt(User user) {
+				// TODO Auto-generated method stub
+				return null;
 			}
 		};
 	}
