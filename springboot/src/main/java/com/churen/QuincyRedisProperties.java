f94e820bb3d152097706b154d703ca34013748e0
Ma Qiang <mq7253180@126.com>
2026-06-26 15:21:02 +0800

    xxx

--- springboot/src/main/java/com/churen/QuincyRedisProperties.java
+++ springboot/src/main/java/com/churen/QuincyRedisProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "spring.redis")
+@ConfigurationProperties(prefix = "redis")
 public class QuincyRedisProperties {
 	private String nodes;
 	private int port = 6379;
